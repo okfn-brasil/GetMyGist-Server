e4e20c529e6a6a022af6f4cc32a36ff763fe3fb3
Duke <duke@riseup.net>
2012-05-01 23:27:01 -0300

    fixed redirect problems

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,12 @@
  */
 
 exports.token = function(req, res){
-  var data = req.query
+  var params = req.query
     , access_token_url = "https://github.com/login/oauth/access_token"
     , request = require('request');
 
 
-  request.post({url: access_token_url+"?client_id="+data.client_id+"&client_secret="+data.client_secret+"&code="+data.code, body: JSON.stringify(data)}, function (e, r, body) {
+  request.post({url: access_token_url+"?client_id="+params.client_id+"&client_secret="+params.client_secret+"&code="+params.code, body: JSON.stringify(params)}, function (e, r, body) {
       var _ref = body.split("&");
 
       if(_ref.length == 1){
@@ -19,7 +19,6 @@ exports.token = function(req, res){
           , token_type = _ref[1].split("=")[1]
           , data = {access_token: access_token, token_type: token_type}
       }
-
-      res.redirect(data.redirect_url+"&access_token="+ access_token +"&token_type=" +token_type);
+      res.redirect(params.redirect_url+"?access_token="+ access_token +"&token_type=" +token_type);
     });
 };
